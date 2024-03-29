6a095804f74a9119b1c74f7b7692956e5e66215f
Ore Adeleye <oreadeleye67@gmail.com>
2024-03-29 19:18:33 +0100

    Initial commit

---
+++ src/user/user.model.ts
@@ -0,0 +1,29 @@
+import { User } from '@/generated/graphql';
+import { model, Schema } from 'mongoose';
+
+const schema = new Schema<User>(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please enter a full name'],
+      index: true,
+    },
+    email: {
+      type: String,
+      lowercase: true,
+      unique: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    lastLogin: {
+      type: Date,
+      default: null,
+    },
+  },
+  { timestamps: true },
+);
+
+export default model<User>('User', schema);
